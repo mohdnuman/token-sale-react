42f9f12a432f0ff6f18105bc9479c1dd37197349
Mohd Numan <mohammadnuman123@gmail.com>
2021-12-20 18:07:58 +0530

    navbar setup

--- token-sale/src/components/App.js
+++ token-sale/src/components/App.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-import { NavbarHeader } from '.';
+import React, { Component } from "react";
+import { NavbarHeader } from ".";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavbarHeader/>
-        
+        <link
+          async
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+        />
+        <NavbarHeader />
+        <script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
       </div>
     );
   }
--- token-sale/src/components/NavbarHeader.js
+++ token-sale/src/components/NavbarHeader.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import GradientButton from "react-linear-gradient-button";
-import { Menu } from "semantic-ui-react";
+import { Menu,Image } from "semantic-ui-react";
 
 class NavbarHeader extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            activeItem:''
+        }
+    }
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render() {
+      const activeItem=this.state;
     return (
       <div
         style={{
@@ -11,16 +19,50 @@ class NavbarHeader extends Component {
           backgroundSize: "cover",
         }}
       >
-        <Menu>
-          <Menu.Item name="Features"  />
-          <Menu.Item name="Use Cases"  />
-          <Menu.Item name="Token Sale"  />
-          <Menu.Item name="Our Mission"  />
-          <Menu.Item name="Contact Us" />
+        <Menu borderless color="#14134f" >
+        <Menu.Item name="Logo"><Image src='/logo.png' size='tiny' /></Menu.Item>
+        <Menu.Item
+          name='Features'
+          active={activeItem === 'Fetures'}
+          onClick={this.handleItemClick}
+        >
+          Editorials
+        </Menu.Item>
+
+        <Menu.Item
+          name='Use Cases'
+          active={activeItem === 'Use Cases'}
+          onClick={this.handleItemClick}
+        >
+          Use Cases
+        </Menu.Item>
+
+        <Menu.Item
+          name='Token Sale'
+          active={activeItem === 'Token Sale'}
+          onClick={this.handleItemClick}
+        >
+         Token Sale
+        </Menu.Item>
+        <Menu.Item
+          name='Our Mission'
+          active={activeItem === 'Our Mission'}
+          onClick={this.handleItemClick}
+        >
+         Our Mission
+        </Menu.Item>
+        <Menu.Item
+          name='Contact Us'
+          active={activeItem === 'Conatact Us'}
+          onClick={this.handleItemClick}
+        >
+         Contact Us
+        </Menu.Item>
         </Menu>
         <br />
         <br />
         <br />
+        fdfd
         <br />
         <br />
         <br />
